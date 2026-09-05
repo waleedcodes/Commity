1589ca6a3dcb84b0c3fe5cceaca5eb58dd6411fe
Waleed Codes <mc@apps-MacBook-Pro.local>
2026-09-05 23:23:08 +0500

    feat(backend): extend User schema with qualification and ranking fields

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -90,6 +90,20 @@ const userSchema = new mongoose.Schema({
     type: Number,
     default: 0,
   },
+  totalContributions: {
+    type: Number,
+    default: 0,
+    index: true,
+  },
+  publicContributions: {
+    type: Number,
+    default: 0,
+    index: true,
+  },
+  privateContributions: {
+    type: Number,
+    default: 0,
+  },
   contributionStreak: {
     type: Number,
     default: 0,
@@ -128,6 +142,7 @@ const userSchema = new mongoose.Schema({
       min: 0,
       max: 100,
     },
+    bytes: Number,
     color: String,
   }],
   
@@ -163,10 +178,16 @@ const userSchema = new mongoose.Schema({
   countryRank: Number,
   cityRank: Number,
   
-  // Last data fetch
+  // Last data fetch & analytics
   lastFetchedAt: {
     type: Date,
     default: Date.now,
+    index: true,
+  },
+  lastAnalyticsUpdate: {
+    type: Date,
+    default: Date.now,
+    index: true,
   },
   
   // Application metadata
@@ -192,20 +213,37 @@ const userSchema = new mongoose.Schema({
 userSchema.index({ username: 1 });
 userSchema.index({ githubId: 1 });
 userSchema.index({ totalCommits: -1 });
+userSchema.index({ totalContributions: -1 });
 userSchema.index({ followers: -1 });
 userSchema.index({ globalRank: 1 });
 userSchema.index({ location: 1 });
 userSchema.index({ lastFetchedAt: 1 });
+userSchema.index({ lastAnalyticsUpdate: 1 });
 userSchema.index({ isActive: 1, isVerified: 1 });
 
-// Compound indexes
-userSchema.index({ totalCommits: -1, followers: -1 });
-userSchema.index({ location: 1, totalCommits: -1 });
+// High performance compound indexes for scaling leaderboards & queries
+userSchema.index({ isActive: 1, totalContributions: -1 });
+userSchema.index({ isActive: 1, totalCommits: -1 });
+userSchema.index({ isActive: 1, followers: -1 });
+userSchema.index({ isActive: 1, publicRepos: -1 });
+userSchema.index({ isActive: 1, longestStreak: -1 });
+userSchema.index({ isActive: 1, location: 1, totalContributions: -1 });
+userSchema.index({ isActive: 1, 'topLanguages.name': 1, totalContributions: -1 });
+userSchema.index({ isActive: 1, lastAnalyticsUpdate: -1 });
 
-// Virtual for total contributions
-userSchema.virtual('totalContributions').get(function() {
-  return this.totalCommits + this.totalPullRequests + this.totalIssues + this.totalReviews;
+// Virtual for GitHub login alias
+userSchema.virtual('login').get(function() {
+  return this.username;
 });
+
+// Virtual for currentStreak alias
+userSchema.virtual('currentStreak')
+  .get(function() {
+    return this.contributionStreak;
+  })
+  .set(function(value) {
+    this.contributionStreak = value;
+  });
 
 // Virtual for account age in days
 userSchema.virtual('accountAge').get(function() {
@@ -228,10 +266,23 @@ userSchema.virtual('profileCompletion').get(function() {
 
 // Pre-save middleware
 userSchema.pre('save', function(next) {
-  // Update lastFetchedAt if any analytics data changed
+  // Always preserve true annual contribution count from GitHub GraphQL if provided,
+  // falling back to sum of components if not set or smaller
+  const calculatedSum = (this.totalCommits || 0) + 
+                        (this.totalPullRequests || 0) + 
+                        (this.totalIssues || 0) + 
+                        (this.totalReviews || 0);
+
+  if (!this.totalContributions || this.totalContributions < calculatedSum) {
+    this.totalContributions = calculatedSum;
+  }
+
+  // Update timestamps if analytics data changed
   if (this.isModified('totalCommits') || this.isModified('totalPullRequests') || 
-      this.isModified('totalIssues') || this.isModified('totalReviews')) {
+      this.isModified('totalIssues') || this.isModified('totalReviews') ||
+      this.isModified('totalContributions')) {
     this.lastFetchedAt = new Date();
+    this.lastAnalyticsUpdate = new Date();
   }
   
   next();
@@ -271,7 +322,11 @@ userSchema.methods.updateRank = async function(category = 'totalCommits') {
 };
 
 userSchema.methods.toPublicJSON = function() {
-  const obj = this.toObject();
+  const obj = this.toObject({ virtuals: true });
+  
+  // Provide compatibility aliases
+  obj.login = obj.username;
+  obj.currentStreak = obj.contributionStreak || 0;
   
   // Remove sensitive information
   delete obj.email;
